83922582ac772118f32824ba5c364603718c541d
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-19 20:05:42 +0100

    design tables

--- model/index.js
+++ model/index.js
@@ -32,3 +32,69 @@ const UserModel = (instance, datatype) => {
 }
 
 const User = UserModel(sequelize, Sequelize);
+
+const HazardModel = (instance, datatype) => {
+  return sequelize.define("hazard", {
+    id: {
+      type: datatype.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true,
+    },
+    location: datatype.GEOMETRY("POINT"),
+    address: datatype.TEXT,
+    city: datatype.STRING,
+    isVerified: {
+      type: datatype.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    blacklisted: { type: datatype.BOOLEAN },
+    state: datatype.STRING,
+    LGA: datatype.STRING,
+    tag: datatype.STRING,
+    fixed: {
+      type: datatype.BOOLEAN,
+      defaultValue: false
+    }
+  });
+}
+
+const Hazard = HazardModel(sequelize, Sequelize);
+
+const ImageModel = (sequelize, datatype) => {
+  return sequelize.define(
+    "image",
+    {
+      id: {
+        type: datatype.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      url: {
+        type: datatype.STRING,
+      },
+      tag: datatype.STRING,
+    },
+    {
+      paranoid: true,
+    }
+  );
+};
+
+const Image = ImageModel(sequelize, Sequelize);
+
+User.hasMany(Hazard);
+Hazard.belongsTo(User);
+Hazard.hasOne(Image);
+Image.belongsTo(Hazard);
+
+sequelize.sync({}).then(() => {
+  console.log(`Database & tables created..`);
+});
+
+module.exports = {
+  User,
+  Hazard
+};
