a06052b39e5a3d72cfdbb8022d7f50890dc528e6
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-30 02:19:12 +0100

    add user routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const userController = require("../controller/user");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function(req, res) {
+	res.send("respond with a resource");
 });
+
+router.post("/verify", userController.verifyUser);
+router.post("/otp", userController.sendOneTimeCode);
+router.post("/verifyOtp", userController.verifyOneTimeCode);
+
 
 module.exports = router;
